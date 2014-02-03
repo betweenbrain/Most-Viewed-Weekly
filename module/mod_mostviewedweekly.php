cc09b6e0e90aab69b0c2ed077e8cea16b677b36d
Matt Thomas <matt@betweenbrain.com>
2014-02-03 13:29:09 -0500

    Fluishing out placeholder files

---
+++ module/mod_mostviewedweekly.php
@@ -0,0 +1,12 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       mostviewedweekly.php
+ * Created    2/3/14 1:23 PM 
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+ 
+ 
